e646e3e0661fdda6a42707c2b4d323346d8f8289
MrParkour <mr-parkour.yt@yandex.ru>
2023-01-16 19:22:26 +0300

    Added animation for running two sides

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 
 
 pygame.init()
-size = width, height = 800, 600
+size = width, height = 600, 400
 screen = pygame.display.set_mode(size)
 
 
@@ -27,7 +27,8 @@ def load_image(name, colorkey=None):
 
 class Person(pygame.sprite.Sprite):
     image = load_image("/idle/adventurer-idle-00.png")
-    im_name = "/idle/adventurer-idle-0"
+    im_name_idle = "/idle/adventurer-idle-0"
+    im_name_run = "/running/adventurer-run-0"
     image_index = 0
 
     def __init__(self, *group):
@@ -35,28 +36,40 @@ class Person(pygame.sprite.Sprite):
         self.image = Person.image
         self.rect = self.image.get_rect()
         self.rect.x = 0
+        self.x_speed = 5
         self.rect.y = 5
         self.action = 'idle'
 
-    def update(self):
+    def update(self, action, tick):
         x, y = 10, 10
-        # if self.np == 'right':
-        #     if self.rect.x <= 440:
-        #         self.rect.x += 2
-        #     else:
-        #         self.image = pygame.transform.flip(self.image, True, False)
-        #         self.np = 'left'
-        # else:
-        #     if self.rect.x >= 10:
-        #         self.rect.x -= 2
-        #     else:
-        #         self.image = pygame.transform.flip(self.image, True, False)
-        #         self.np = 'right'
+        if action == "idle":
+            self.action = "idle"
+            self.next_animation(tick)
+        if action == "right":
+            self.action = "right"
+            if self.rect.x <= 550:
+                self.rect.x += self.x_speed
+            self.next_animation(tick)
+        if action == "left":
+            self.action = "left"
+            if self.rect.x >= 20:
+                self.rect.x -= self.x_speed
+            self.next_animation(tick)
 
-    def next_animation(self):
+    def next_animation(self, tick):
         if self.action == "idle":
-            self.image_index += 1
-            self.image = load_image(self.im_name + str(self.image_index % 3) + ".png")
+            if tick % 15 == 0:
+                self.image_index += 1
+                self.image = load_image(self.im_name_idle + str(self.image_index % 3) + ".png")
+        elif self.action == "right":
+            if tick % 10 == 0:
+                self.image_index += 1
+                self.image = load_image(self.im_name_run + str(self.image_index % 6) + ".png")
+        elif self.action == "left":
+            if tick % 10 == 0:
+                self.image_index += 1
+                self.image = load_image(self.im_name_run + str(self.image_index % 6) + ".png")
+                self.image = pygame.transform.flip(self.image, True, False)
 
 
 if __name__ == "__main__":
@@ -71,14 +84,20 @@ if __name__ == "__main__":
     clock = pygame.time.Clock()
 
     anim_change_tick = 0
+    action = "idle"
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        if anim_change_tick % 15 == 0:
-            prs.next_animation()
-        prs.update()
+            if event.type == pygame.KEYDOWN:
+                if event.key == 276:
+                    action = "left"
+                if event.key == 275:
+                    action = "right"
+            if event.type == pygame.KEYUP:
+                action = "idle"
+        prs.update(action, anim_change_tick)
         screen.fill(pygame.Color(255, 255, 255))
         all_sprites.draw(screen)
         clock.tick(60)
